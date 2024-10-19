0833b92c23119dbfc495bc63018d33a2cbe09f32
Maloth-12 <vitalprem24@gmail.com>
2024-10-19 11:09:06 -0400

    new resume is uploaded

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,10 +3,9 @@ import HeroImage from '../assets/vitalpremkumar.jpeg'
 
 const Hero = () => {
   return (
-    <div className='bg-black text-white text-center py-8'>
+    <div className='py-8 text-center text-white bg-black'>
         <img src={HeroImage} alt="" 
-        className='mx-auto mb-8 w-48 h-48 rounded-full object-cover transform 
-        transition-transform duration-300 hover:scale-105'/>
+        className='object-cover w-48 h-48 mx-auto mb-8 transition-transform duration-300 transform rounded-full hover:scale-105'/>
         <h1 className='text-4xl font-bold'>
             I'm {" "}
             <span className=' bg-clip-text bg-gradient-to-r from-green-400 to-blue-500'>Vital Prem Kumar Maloth</span>
@@ -17,14 +16,12 @@ const Hero = () => {
         </p>
         <div className='mt-8 space-x-4'>
             <button
-            className='bg-gradient-to-r from-green-400 to-blue-500 text-white
-            transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
+            className='px-4 py-2 text-white transition-transform duration-300 transform rounded-full bg-gradient-to-r from-green-400 to-blue-500 hover:scale-105'>
               <a href="#contact" className='hover:text-gray-400'>Contact Me</a>
             </button>
             <button
-            className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
-            transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>
-              <a href="/resume.pdf" download="Maloth_resume.pdf" className='hover:text-gray-400'>Resume</a>
+            className='px-4 py-2 text-white transition-transform duration-300 transform rounded-full bg-gradient-to-r from-pink-500 to-yellow-500 hover:scale-105'>
+              <a href="/Vital Software Engineer Resume.pdf" download="Maloth_resume.pdf" className='hover:text-gray-400'>Resume</a>
             </button>
         </div>
 
